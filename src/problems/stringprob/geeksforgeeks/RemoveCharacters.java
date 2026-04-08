0b43752d4d5980ff307c53e202bedb799141d5dc
dharunr <dharun.r.partner@decathlon.com>
2026-04-08 13:10:58 +0530

    fix

--- src/problems/stringprob/geeksforgeeks/RemoveCharacters.java
+++ src/problems/stringprob/geeksforgeeks/RemoveCharacters.java
@@ -17,7 +17,25 @@ public class RemoveCharacters {
         return result;
     }
 
+    public static String removeChars1(String s1, String s2) {
+
+        String result = "";
+
+        for (int i = 0; i < s1.length(); i++) {
+
+            char ch = s1.charAt(i);
+
+            if (s2.indexOf(ch) == -1) {
+                result += ch;
+            }
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         System.out.println(removeChars("computer", "cat")); // ompuer
+        System.out.println(removeChars1("computer", "cat")); // ompuer
+
     }
 }
